a82adbf8840ef82710035bf0bd53e38e92e72b4e
Timothy Fraser, PhD <timothy.fraser.1@gmail.com>
2024-07-15 15:48:58 -0400

    Update example.R

--- data/japan/example.R
+++ data/japan/example.R
@@ -2,5 +2,23 @@
 #' @description
 #' Example script for how to use data in this folder.
 
+library(dplyr)
+library(readr)
+library(sf)
+library(ggplot)
+
+# Load municipality polygons
+muni = read_sf("data/japan/municipalities.geojson")
+
+# Filter to just municipalities in Hokkaido prefecture
+subset = muni %>%
+  filter(pref == "Hokkaido")
+
+ggplot()  +
+  geom_sf(data = subset)
 
 
+# Same idea applies for:
+pref = read_sf("data/japan/prefectures.geojson")
+regions = read_sf("data/japan/regions.geojson")
+country = read_sf("data/japan/country.geojson")
