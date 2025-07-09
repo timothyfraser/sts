930b10a037aa09ddb33cd4a825837a243e843206
Timothy Fraser, PhD <timothy.fraser.1@gmail.com>
2025-07-09 13:28:44 -0400

    Create make_database.R

---
+++ data/air_quality/make_database.R
@@ -0,0 +1,34 @@
+# make_database.R
+
+# Need to make this into a database?
+
+library(dplyr) # for data wrangling
+library(readr) # for reading data
+library(DBI) # for database connections
+library(RSQlite) # for SQLite
+library(dbplyr) # for database SQL queries with dplyr
+
+
+# Create a database connection object at this path
+db = dbConnect(RSQLite::SQLite(), "mydb.sqlite")  
+
+# Get an empty data.frame with the right headers
+headers = read_csv("data/air_quality/air_quality.csv", n_max = 0)
+
+# Initialize the table and field/column types
+dbWriteTable(conn = db, name = "air_quality", value = headers, row.names = FALSE)
+
+# Load data into memory, in chunks if needed
+data = read_csv("data/air_quality/air_quality.csv")
+
+# Append the data into the table, in chunks if needed
+dbWriteTable(conn = db, name = "air_quality", value = data, append = TRUE)
+
+# Test query the data
+db %>% tbl("air_quality") %>% head() %>% collect()
+
+# Disconnect from the database
+dbDisconnect(db)
+
+# Cleanup
+rm(list = ls())
